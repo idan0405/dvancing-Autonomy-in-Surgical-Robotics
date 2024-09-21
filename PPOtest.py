750f2914e401aeaeb6c2f4a0bd6f6860f2614470
idan0405 <tigerglassberg@gmail.com>
2024-09-21 21:06:33 +0300

    update

--- PPOtest.py
+++ PPOtest.py
@@ -7,8 +7,19 @@ from utils import *
 env=gym.make('NeedleReach-v0', render_mode='human')
 env=TimeLimit(env, max_episode_steps=50)
 env=RGBWrapper(env)
-model=RecurrentPPO.load("ppo-reach")
-
+obs = env.reset()
+model = RecurrentPPO(
+    RecurrentPPOMultiInputPolicy,
+    env,
+    n_steps=2048,
+    batch_size=256,
+    n_epochs=4,
+    learning_rate=lambda t: t * 2e-4,
+    clip_range=lambda t: t * 0.2,
+    ent_coef=0.01,
+    tensorboard_log=f"runs/{time.time()}_reach",
+)
+model.set_parameters("ppo-reach")
 lstm_states = None
 episode_starts = np.ones(1, dtype=bool)
 ep=1.0
@@ -18,7 +29,6 @@ while True:
     obs, rewards, done, info = env.step(action)
     episode_starts=done
     time.sleep(0.025)
-
     if done:
         if info['is_success']:
             suc+=1.0
